e34b92cf1fc1871a1217f8793900639cced4a705
Evgheni Barducov <52833790+baovevni@users.noreply.github.com>
2024-05-08 17:40:01 -0400

    implemented asyncBreedsTest.js

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -1,18 +1,24 @@
 // asyncBreeds.js
 const fs = require('fs');
 
-const breedDetailsFromFile = function(breed, callback) {
+const breedDetailsFromFile = function(breed, functionToRunWhenThingsAreDone) {
   console.log('breedDetailsFromFile: Calling readFile...');
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    if (error){
+      return functionToRunWhenThingsAreDone(undefined);
+    }
+    // CHANGE: Pass data into callback instead of returning it directly
     console.log("In readFile's Callback: it has the data.");
-    // ISSUE: Returning from *inner* callback function, not breedDetailsFromFile.
-    if (!error) callback(data);
+    if (!error) functionToRunWhenThingsAreDone(data);
   });
-  // ISSUE: Attempting to return data out here will also not work.
-  //        Currently not returning anything from here, so breedDetailsFromFile function returns undefined.
 };
 
-const printCatBreed = breed => {
-  console.log('Return Value: ', breed);
-}
-breedDetailsFromFile('Bombay', printCatBreed);
+// CHANGE 1: Moved the console.log into a new function:
+const printOutCatBreed = breed => {
+  console.log('Return Value: ', breed) // => print out details correctly.
+};
+
+// CHANGE 2: we're now passing two arguments into breedDetailsFromFile: breed string and a callback function
+breedDetailsFromFile('Bombay', printOutCatBreed);
+
+module.exports = breedDetailsFromFile;
